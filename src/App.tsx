056550f33ed91b2bdbb78787da462adf873c3597
Mikita Kamiakevich <79355999+nikitsch@users.noreply.github.com>
2023-06-02 17:46:49 +0300

    feat/redux toolkit slice and working out standard scenario (#2)
    * feat: ActionCreators and UserSlice
    
    * feat: waorking out a standard scenario; extraReducers; createAsyncThunk

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
+import { useAppDispatch, useAppSelector } from './hooks/redux';
+import { fetchUsers } from './store/reducers/ActionCreators';
 
 function App() {
+  const dispatch = useAppDispatch()
+  const {users, isLoading, error} = useAppSelector(state => state.userReducer)
+  
+  useEffect(() => {
+    dispatch(fetchUsers())
+  }, [ ])
+
   return (
     <div className="App">
-      dgrg
+      {isLoading && <h1>Loading...</h1>}
+      {error && <h1>{error}</h1>}
+      {users.length ? JSON.stringify(users, null, 2) : ""}
     </div>
   );
 }
---
+++ src/store/reducers/UserSlice.ts
@@ -0,0 +1,52 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { IUser } from "../../models/IUser"
+import { fetchUsers } from "./ActionCreators";
+
+interface UserState {
+  users: IUser[];
+  isLoading: boolean;
+  error: string;
+  count: number;
+}
+
+const initialState: UserState = {
+  users: [],
+  isLoading: false,
+  error: "",
+  count: 0
+}
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    // usersFatching(state) {
+    //   state.isLoading = true
+    // },
+    // usersFatchingSuccess(state, action: PayloadAction<IUser[]>) {
+    //   state.isLoading = false
+    //   state.error = ""
+    //   state.users = action.payload
+    // },
+    // usersFatchingError(state, action: PayloadAction<string>) {
+    //   state.isLoading = false
+    //   state.error = action.payload
+    // }
+  },
+  extraReducers: {
+    [fetchUsers.fulfilled.type]: (state, action: PayloadAction<IUser[]>) => {
+      state.isLoading = false
+      state.error = ""
+      state.users = action.payload
+    },
+    [fetchUsers.pending.type]: (state) => {
+      state.isLoading = true
+    },
+    [fetchUsers.rejected.type]: (state, action: PayloadAction<string>) => {
+      state.isLoading = false
+      state.error = action.payload
+    }
+  }
+})
+
+export default userSlice.reducer
