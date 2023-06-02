1c0881fae2d202c559bb9db1b1f7d3bf09d6c85b
nikitsch <nikitsch5@gmail.com>
2023-06-02 16:36:31 +0300

    feat: ActionCreators and UserSlice

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useAppDispatch, useAppSelector } from './hooks/redux';
+import { userSlice } from './store/reducers/UserSlice';
 
 function App() {
+  const dispatch = useAppDispatch()
+  
   return (
     <div className="App">
-      dgrg
+      wazzap dude
     </div>
   );
 }
---
+++ src/store/reducers/UserSlice.ts
@@ -0,0 +1,26 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { IUser } from "../../models/IUser"
+
+interface UserState {
+  users: IUser[];
+  isLoading: boolean;
+  error: string;
+  count: number;
+}
+
+const initialState: UserState = {
+  users: [],
+  isLoading: false,
+  error: "",
+  count: 0
+}
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+
+  }
+})
+
+export default userSlice.reducer
