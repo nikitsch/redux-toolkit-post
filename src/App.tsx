7837976386f3be8fc3e55d89c9cd92a688fbf8ca
Mikita Kamiakevich <79355999+nikitsch@users.noreply.github.com>
2023-06-03 18:41:47 +0300

    feat: post component with RTK-query (#3)

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
 import { useEffect } from 'react';
 import './App.css';
+import PostContainer from './components/PostContainer';
 import { useAppDispatch, useAppSelector } from './hooks/redux';
 import { fetchUsers } from './store/reducers/ActionCreators';
 
 function App() {
-  const dispatch = useAppDispatch()
-  const {users, isLoading, error} = useAppSelector(state => state.userReducer)
+  // const dispatch = useAppDispatch()
+  // const {users, isLoading, error} = useAppSelector(state => state.userReducer)
   
-  useEffect(() => {
-    dispatch(fetchUsers())
-  }, [ ])
+  // useEffect(() => {
+  //   dispatch(fetchUsers())
+  // }, [ ])
 
   return (
     <div className="App">
-      {isLoading && <h1>Loading...</h1>}
+      {/* {isLoading && <h1>Loading...</h1>}
       {error && <h1>{error}</h1>}
-      {users.length ? JSON.stringify(users, null, 2) : ""}
+      {users.length ? JSON.stringify(users, null, 2) : ""} */}
+      <PostContainer />
     </div>
   );
 }
