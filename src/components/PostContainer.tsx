ce6d2894e1a3e5b3081e2c8656017326c4c99141
Mikita Kamiakevich <79355999+nikitsch@users.noreply.github.com>
2023-06-05 17:55:54 +0300

    feat/mutation (#4)
    * feat: json server on localhost:5000
    
    * feat: add post
    
    * feat: update and delete post

--- src/components/PostContainer.tsx
+++ src/components/PostContainer.tsx
@@ -1,16 +1,38 @@
+import { IPost } from "../models/IPost"
 import { postAPI } from "../services/PostService"
 import PostItem from "./PostItem"
 
 const PostContainer = () => {
-  const { data: posts, error, isLoading } = postAPI.useFetchAllPostsQuery(10)
+  const { data: posts, error, isLoading } = postAPI.useFetchAllPostsQuery(100)
+  const [createPost, { }] = postAPI.useCreatePostMutation()
+  const [updatePost, { }] = postAPI.useUpdatePostMutation()
+  const [deletePost, { }] = postAPI.useDeletePostMutation()
+
+  const handleCreate = async () => {
+    const title = prompt()
+    await createPost({ title, body: title } as IPost)
+  }
+
+  const handleRemove = (post: IPost) => {
+    deletePost(post)
+  }
+
+  const handleUpdate = (post: IPost) => {
+    updatePost(post)
+  }
 
   return (
     <div>
       <div className="post__list">
+        <button onClick={handleCreate}>Add new post</button>
         {isLoading && <h1>Loading...</h1>}
         {error && <h1>Failed to load user(</h1>}
-        {posts && posts.map(post => 
-          <PostItem post={post} key={post.id} />)}
+        {posts && posts.map(post =>
+          <PostItem
+            post={post}
+            remove={handleRemove}
+            update={handleUpdate}
+            key={post.id} />)}
       </div>
     </div>
   )
